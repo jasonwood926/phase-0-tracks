75b10ed2b0b07126024173ddcbb5a12c2866ccd7
Jason Wood <jasonwood926@gmail.com>
2016-12-13 17:28:14 -0800

    Solved all problems in method detective file

---
+++ ruby/method_detective1.rb
@@ -0,0 +1,39 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+p "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
+
+p "zom".insert(2, "o")
+# => “zoom”
+
+p "enhance".center(15)
+# => "    enhance    "
+
+p "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
+
+"the usual".insert(-1, " suspects")
+#=> "the usual suspects"
+
+p" suspects".insert(0, "the usual")
+# => "the usual suspects"
+
+p "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
+
+p "The mystery of the missing first letter".slice(1..-1)
+# => "he mystery of the missing first letter"
+
+p "Elementary,    my   dear        Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
+
+p "z".ord
+# => 122 
+# 122 corresponds with the letter "z" as its ordinal in the American Standard Code for Information Interchange"
+
+p "How many times does the letter 'a' appear in this string?".count("a")
+# => 4
